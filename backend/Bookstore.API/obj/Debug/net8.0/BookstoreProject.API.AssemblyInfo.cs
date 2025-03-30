01edfd76d8c51974a09611a36c49f5505ab299aa
emmajean720 <167582619+emmajean720@users.noreply.github.com>
2025-03-29 20:01:23 -0600

    updates more

--- backend/Bookstore.API/obj/Debug/net8.0/BookstoreProject.API.AssemblyInfo.cs
+++ backend/Bookstore.API/obj/Debug/net8.0/BookstoreProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099500740f39a8894d1ca8f1d0579264ebfcc1d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31c0f5ff4642d1afcd706bb00d3dd2370aafb4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
